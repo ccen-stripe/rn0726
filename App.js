5bfaba839b423aa649bc2d5a7e5f064197404ba5
Chen Cen <ccen@stripe.com>
2024-01-19 15:20:19 -0800

    covert to prebuild, failing expoCMake

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React, { useState, useCallback } from 'react';
+import {
+  useStripeIdentity,
+  IdentityVerificationSheetOptions,
+} from '@stripe/stripe-identity-react-native';
+
+import logo from './assets/logo.png';
+
 export default function App() {
+
+
+  const [options, setOptions] = useState<VerificationSessionOptions>({
+    requireMatchingSelfie: false,
+    requireIdNumber: false,
+    allowedTypes: {
+      ['driving_license']: true,
+      ['id_card']: true,
+      ['passport']: true,
+    },
+    requireLiveCapture: false,
+  });
+
+  const fetchOptions = useCallback(async () => {
+    return {
+      sessionId: 'vs_1M73GDGMZYGNxJkBXchPkSD1',
+      ephemeralKeySecret: 'ek_live_YWNjdF8xSDM0ZFhHTVpZR054SmtCLHRJc21CVEJYVDl5Y1lMNnpTVnBlVmhidVVLcVhyUlM_00uOuclWPR',
+      brandLogo: Image.resolveAssetSource(logo),
+    };
+  }, [options]);
+
+  const { status, present, loading } = useStripeIdentity(fetchOptions);
+
+  const handlePress = useCallback(() => {
+    present();
+  }, [present]);
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>Hellfdo</Text>
+      <Button
+        testID="verify-btn"
+        title="Verify Identity"
+        onPress={handlePress}
+      />
       <StatusBar style="auto" />
     </View>
   );
